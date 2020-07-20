944a3bf44f2f59aa627dae36fecc3fd3a50cc232
cwelshofer <cwelshofer@gmail.com>
2020-07-20 14:00:27 -0500

    headscode

---
+++ scripts/canada/heads/HeadsDataProvider.js
@@ -0,0 +1,14 @@
+const Heads = [
+    
+    {head: "Toronto"},
+    {head: "Vancouver"},
+    {head: "Calgary"},
+    {head: "Quebec"},
+    {head: "Charlottetown"}
+  
+]
+
+
+export const allHeads = () => {
+  return Heads.slice()   
+}

4a6f6fedcec9b45061d3218f2661c7e8c0fb883a
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-20 15:49:47 -0500

    working on adding more properties to obj

--- scripts/italy/cities/CityHTMLConverter.js
+++ scripts/italy/cities/CityHTMLConverter.js
@@ -1,5 +1,7 @@
 export const citiesHTML = (citiesObj) => {
     return `
         <div class ="city">${citiesObj.name}</div>
+        <div><img class="city__image" src="${citiesObj.image}"></div>
+        <a class= "city__link" target=_blank href="${citiesObj.URL}">Click Here for more info</a>
         `
 }
--- scripts/italy/landmarks/LandmarkHTMLConverter.js
+++ scripts/italy/landmarks/LandmarkHTMLConverter.js
@@ -1,5 +1,7 @@
 export const landmarksHTML = (landmarkObj) => {
     return `
-    <div class="landmark">${landmarkObj.name}</div
-    `
+    <div class ="landmark">${landmarkObj.name}</div>
+        <div><img class="landmark__image" src="${landmarkObj.image}"></div>
+        <a class= "landmark__link" target=_blank href="${landmarkObj.URL}">Click Here for more info</a>
+        `
 }

207400cb432cc7be75320eb71f9f41838cb7179b
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-21 10:40:20 -0500

    playing around with styling

--- scripts/italy/cities/CityHTMLConverter.js
+++ scripts/italy/cities/CityHTMLConverter.js
@@ -2,7 +2,8 @@ export const citiesHTML = (citiesObj) => {
     return `
         <div class ="city">${citiesObj.name}</div>
         <div><img class="city__image" src="${citiesObj.image}"></div>
-        <a class= "city__link" target=_blank href="${citiesObj.URL}">Click Here for more info</a>
+        <div class="city__description">Description PROPERTY goes here!<div>
+        <a class= "city__link" target=_blank href="${citiesObj.URL}">Traveling to ${citiesObj.name}</a>
         <br></br>
         `
 }

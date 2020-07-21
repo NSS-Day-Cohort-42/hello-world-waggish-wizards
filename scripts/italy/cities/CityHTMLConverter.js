28c94cf19924be0c2c66eb0ca45aff2444830c30
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-21 11:16:01 -0500

    added hover over feature

--- scripts/italy/cities/CityHTMLConverter.js
+++ scripts/italy/cities/CityHTMLConverter.js
@@ -3,9 +3,8 @@ export const citiesHTML = (citiesObj) => {
         <div class ="city">${citiesObj.name}</div>
         <section class="city__hover">
         <div><img class="city__image" src="${citiesObj.image}"></div>
-        <div class="city__description">Description PROPERTY goes here!<div>
+        <div class="city__link"><a target=_blank href="${citiesObj.URL}">Click to learn more about traveling to ${citiesObj.name}</a></div>
         </section>
-        <a class= "city__link" target=_blank href="${citiesObj.URL}">Traveling to ${citiesObj.name}</a>
         <br></br>
         `
 }

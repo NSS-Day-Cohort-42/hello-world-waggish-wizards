a7188e339940689924d539ca750ea4f496b18f0d
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-21 10:56:27 -0500

    playing with more features

--- scripts/italy/cities/CityHTMLConverter.js
+++ scripts/italy/cities/CityHTMLConverter.js
@@ -1,8 +1,10 @@
 export const citiesHTML = (citiesObj) => {
     return `
         <div class ="city">${citiesObj.name}</div>
+        <section class="city__hover">
         <div><img class="city__image" src="${citiesObj.image}"></div>
         <div class="city__description">Description PROPERTY goes here!<div>
+        </section>
         <a class= "city__link" target=_blank href="${citiesObj.URL}">Traveling to ${citiesObj.name}</a>
         <br></br>
         `
